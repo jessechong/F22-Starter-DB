7d1a9eec9fbc072f9f680eb40b2d4d39a6cfdea8
Jesse Chong <jec334@gmail.com>
2022-09-27 01:02:25 -0700

    feat: Updated readme, gitignore, and SQL statement

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # F22-Starter-DB
 Simple starter database project for F22 Columbia classes.
+
+# Commands
+Can start MySQL server through these steps:
+- Start -> cmd.exe -> services.msc -> MySQL -> start
+- Source: https://stackoverflow.com/a/20167008
--- some_columbia_student_sql.sql
+++ some_columbia_student_sql.sql
@@ -23,3 +23,5 @@ create table columbia_student
  insert into columbia_student (last_name, first_name, middle_name, email, uni)
     values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
 
+ insert into columbia_student (last_name, first_name, middle_name, email, uni)
+    values ("Chong", "Jesse", "Lin", "jlc2332@columbia.edu", "jlc2332");
